3e7d39b18e932345a75a0a20c3be677e7f9d9ae4
Lucas Franchini <lucasfranchini.a@gmail.com>
2021-04-20 18:10:59 -0300

    criação da função para abrir a pagina de quizz e começo da dunção de popular perguntas

--- scripts.js
+++ scripts.js
@@ -26,4 +26,43 @@ function abrirQuizz(quizz){
 
 function criarPaginaQuizz(resposta){
     console.log(resposta)
+    const paginaQuizz = document.querySelector(".pagina-quizz-aberto");
+    paginaQuizz.classList.remove("escondido");
+    const background = `
+    background: linear-gradient(0deg, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url("${resposta.data.image}");
+    background-size: cover;`;
+    paginaQuizz.innerHTML = `
+    <div class="cabecalho-quizz" style = '${background}'>
+        <h1>${resposta.data.title}</h1>
+    </div>`;
+    const perguntas = resposta.data.questions;
+    console.log(perguntas);
+    perguntas.forEach(popularPerguntas);
+}
+
+function popularPerguntas(pergunta){
+    const paginaQuizz = document.querySelector(".pagina-quizz-aberto");
+    paginaQuizz.innerHTML +=`
+    <div class="pergunta">
+        <div class="titulo-pergunta" style="background-color:${pergunta.color}">
+            <span>${pergunta.title}</span>
+        </div>
+        <div class="caixa-respostas">
+            <div class="resposta">
+
+            </div>
+            <div class="resposta">
+            </div>
+            
+            <div class="resposta">
+            </div>
+            
+            <div class="resposta">
+            </div>
+        </div>
+    </div>`;
+}
+
+function comparador() { 
+	return Math.random() - 0.5; 
 }
