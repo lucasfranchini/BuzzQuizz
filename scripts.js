ed839109eb3fad91013461f5c2a120dc771ae698
Thiago Pereira <thiago.spereira02@gmail.com>
2021-04-22 21:52:05 -0300

    Alterando verificaçoes e adicionando funçoes para tela de finalizaçao

--- scripts.js
+++ scripts.js
@@ -152,21 +152,26 @@ function comparador() {
 
 
 
-
+let tituloQuizz = "";
+let urlTitulo = "";
 let qnt_perguntas = 0;
 let niveis = 0;
+const dados = [];
 
 function prosseguirParaPerguntas(){
-    const titulo = document.querySelector(".titulo").value;
-    const url = document.querySelector(".url").value
+    tituloQuizz = document.querySelector(".titulo").value;
+    urlTitulo = document.querySelector(".url").value
     qnt_perguntas = parseInt(document.querySelector(".quantidadePerguntas").value);
     niveis = parseInt(document.querySelector(".quantidadeNiveis").value);
 
-    if (!validateURL(url) || titulo.length<20 || titulo.length>65 || qnt_perguntas<3 || niveis<2){
+    if (!validateURL(urlTitulo) || tituloQuizz.length<20 || tituloQuizz.length>65 || qnt_perguntas<3 || niveis<2){
         alert("Preencha os dados corretamente");
     } else{
         irParaPerguntas();
     }
+
+    dados.title = tituloQuizz;
+    dados.image = urlTitulo;
 }
 
 function irParaPerguntas(){
@@ -194,19 +199,19 @@ function popularPerguntas(){
                     <input type="text" class="cor" placeholder="Cor de fundo da pergunta" onfocus="this.value='';">    
                     <h2>Resposta correta</h2>
                     <input type="text" class="correta" placeholder="Resposta correta" onfocus="this.value='';" required>
-                    <input type="text" class="urlResposta" placeholder="URL da imagem" onfocus="this.value='';" required> 
+                    <input type="text" class="urlRespostaCorreta" placeholder="URL da imagem" onfocus="this.value='';" required> 
                     <h2>Respostas incorretas</h2>
                     <div class="resposta-incorreta">
-                        <input type="text" class="incorrreta1" placeholder="Resposta incorreta 1" onfocus="this.value='';">
-                        <input type="text" placeholder="URL da imagem 1" onfocus="this.value='';"> 
+                        <input type="text" class="incorreta1" placeholder="Resposta incorreta 1" onfocus="this.value='';">
+                        <input type="text" class="urlIncorreta1" placeholder="URL da imagem 1" onfocus="this.value='';"> 
                     </div>
                     <div class="resposta-incorreta">
-                        <input type="text" class="incorrreta2" placeholder="Resposta incorreta 2" onfocus="this.value='';">
-                        <input type="text" placeholder="URL da imagem 2" onfocus="this.value='';"> 
+                        <input type="text" class="incorreta2" placeholder="Resposta incorreta 2" onfocus="this.value='';">
+                        <input type="text" class="urlIncorreta2" placeholder="URL da imagem 2" onfocus="this.value='';"> 
                     </div>
                     <div class="resposta-incorreta">
-                        <input type="text" class="incorrreta3" placeholder="Resposta incorreta 3" onfocus="this.value='';">
-                        <input type="text" placeholder="URL da imagem 3" onfocus="this.value='';"> 
+                        <input type="text" class="incorreta3" placeholder="Resposta incorreta 3" onfocus="this.value='';">
+                        <input type="text" class="urlIncorreta3" placeholder="URL da imagem 3" onfocus="this.value='';"> 
                     </div>
                 </div>
             </div>
@@ -228,12 +233,15 @@ function abrirMenuDados(perguntaAberta){ // pega o elemento que voce clicou, que
 }
 
 function prosseguirParaNiveis(){
+    const questions = [];
+
     for(let i=1; i<=qnt_perguntas; i++){
         const pergunta = document.querySelector(".pergunta"+i);
         const texto = pergunta.querySelector(".dados-pergunta .texto").value;
         const cor = pergunta.querySelector(".dados-pergunta .cor").value;
         const respostaCorreta = pergunta.querySelector(".dados-pergunta .correta").value;
-        const urlResposta = pergunta.querySelector(".dados-pergunta .urlResposta").value;
+        const urlRespostaCorreta = pergunta.querySelector(".dados-pergunta .urlRespostaCorreta").value;
+        const answers = [];
 
         if (texto.length<20){
             alert("Preencha os dados corretamente");
@@ -250,7 +258,7 @@ function prosseguirParaNiveis(){
             return;
         }
 
-        if (!validateURL(urlResposta)){
+        if (!validateURL(urlRespostaCorreta)){
             alert("Preencha os dados corretamente");
             return;
         }
@@ -259,7 +267,39 @@ function prosseguirParaNiveis(){
             alert("Preencha os dados corretamente");
             return;
         }
+
+        answers.push({
+                text: respostaCorreta, 
+                image: urlRespostaCorreta,
+                isCorrectAnswer: true
+        })
+        
+        for (let z=1; z<=3; z++){
+            const respostaIncorreta = pergunta.querySelector(".incorreta"+i).value;
+            const urlIncorreta = pergunta.querySelector(".urlIncorreta"+i).value;
+
+            if (respostaIncorreta !== ""){
+                if (!validateURL(urlIncorreta)){
+                    alert("Preencha os dados corretamente");
+                    return;        
+                }
+
+                answers.push({
+                    text: respostaIncorreta,
+                    image: urlIncorreta,
+                    isCorrectAnswer: false
+                })
+            }
+        }
+        questions.push({
+            title: texto,
+            color: cor,
+            answers: answers
+        })
     }
+
+    dados.questions = questions;
+    console.log(dados)
 
     irParaNiveis();
 }
@@ -311,6 +351,8 @@ function abrirMenuNiveis(nivelAberto){
 }
 
 function finalizarQuizz(){
+    const levels = [];
+
     for (i=1; i<=niveis; i++){
         const nivel = document.querySelector(".nivel"+i);
         const titulo = nivel.querySelector(".titulo").value;
@@ -340,19 +382,66 @@ function finalizarQuizz(){
             alert("Preencha os dados corretamente");
             return;
         }
+    
+        levels.push({
+            title: titulo,
+            image: urlImagem,
+            text: descricao,
+            minValue: porcentagem
+        })
     }
     
-    for (i=1; i<=niveis; i++){
-        if (porcentagens[i]!==0){
-            alert("Preencha os dados corretamente");
-            return;
-        }
+    if (porcentagens.indexof(0)===-1){
+        alert("Preencha os dados corretamente");
+        return;
     }
+
+    dados.levels = levels;
+
+    const promessa = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/buzzquizz/quizzes", dados);
+    promessa.then(executado);
+    promessa.catch(erro);
 
     irParaFinalizacao();
 }
 
+function executado(){
 
+}
+
+function erro(){
+    alert("Ocorreu um erro no envio do seu quizz");
+}
+
+function irParaFinalizacao(){
+    const pagNiveis = document.querySelector(".definir-niveis");
+    pagNiveis.classList.add("escondido");
+    const pagFinalizacao = document.querySelector(".finalizar-criacao");
+    pagFinalizacao.classList.remove("escondido");
+
+    popularFinalizacao();
+}
+
+function popularFinalizacao(){
+    const finalizar = document.querySelector(".finalizar-criacao div");
+    finalizar.innerHTML = `
+        <img src="${quizzes[i].image}">
+        <div class="degrade"></div>
+        <span>${quizzes[i].title}</span>
+    `;
+}
+
+function acessarQuizz(){
+    
+}
+
+function voltarHome(){
+    const pagFinalizacao = document.querySelector(".finalizar-criacao");
+    pagFinalizacao.classList.add("escondido");
+    const pagInicial = document.querySelector(".pagina-inicial");
+    pagInicial.classList.remove("escondido");
+    pegarquizzes();
+}
 
 
 
